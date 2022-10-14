d5b817c9ab880c6947f4b1a928e91a3561dc155a
local_admin <t@h.com>
2022-10-14 13:14:04 +0100

    Massive Re-base into a functions file.

--- basic_plotting.py
+++ basic_plotting.py
@@ -1,83 +1,25 @@
 # REVIEW: putting exception handling around imports suggests that initialisation issues are not resolved
 try:
-    import datetime
     import os
-    import csv
     import sys
     import matplotlib.pyplot as plt
     import matplotlib
     import numpy as np
+    import functions
 except ImportError as e:
     sys.exit("Importing error: " + str(e))
 
 default_file_name = "testing/test_data_from_counter.csv"
-global_dict = []
+# can I remove this global variable?
 
 
-def sort_dates(input_list):
-    print("input list: ", input_list)
-    date_output_list = []
-    for i in input_list:
-        date_output_list.append(datetime.datetime(int(i[0] + i[1]),
-                                                  int(i[3] + i[4]),
-                                                  int(i[6] + i[7]),
-                                                  int(i[9] + i[10])))
-    date_output_list.sort()  # this should go in ascending order
-    return date_output_list
-
-
-# find and open data file
-def open_file(input_value):
-    output = ""
-    try:
-        output = open(input_value, "r")
-    except Exception as err:
-        print("error opening file: ", err)
-    if output is not None:
-        pass
-    else:
-        output = "Error"  # need a better handle than this
-    return output
-
-
-# read in data from csv file
-def read_in_data():
-    file = open_file(default_file_name)
-    reader = csv.reader(file)
-    for each_row in reader:
-        global_dict.append(each_row)
-    file.close()
-
-def reformat_data():
-    """
-    This will take data in str format "YY-MM-DD HH" and return into (datetime, str)
-    :return: list -> WeatherData(datetime, str)
-    """
-    local_x = []
-    local_y = []
-    output = []
-
-    for gl in global_dict:
-        for counter, g in enumerate(gl):
-            if counter % 2 == 0:
-                temp = g.replace('"', '')
-                t = str(temp.lstrip())
-                tmp = str(t[0:2]) + str(t[3:5]) + str(t[6:8])
-                local_x.append(tmp)
-                # print("stuff: {}, {}, {}, hour {}".format(t[0:2], t[3:5], t[6:8], t[9:11]))
-            else:
-                local_y.append(str(g))
-        #output = sort_dates(local_x) # this takes a string yymmdd and returns a datetime format.
-    return local_x, local_y
-
-
-def basic_plot():
+def basic_plot(input_list):
     """
     This functions takes dates (and hours) and plots them on a basic x-y chart.
     """
 
     # even to x, odd to y
-    dates, y_values = reformat_data()
+    dates, y_values = functions.reformat_data(input_list)
 
     x = np.array(dates)
     y = np.array(y_values)
@@ -91,5 +33,5 @@ def basic_plot():
 
 
 if __name__ == '__main__':
-    read_in_data()
-    basic_plot()
+    alist = functions.read_in_data(default_file_name)
+    basic_plot(alist)
---
+++ functions.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+"""A collection of all functions for this program."""
+try:
+    import datetime
+    import os
+    import sys
+    import csv
+    import matplotlib.pyplot as plt
+    import matplotlib
+    import numpy as np
+except ImportError as e:
+    sys.exit("Importing error: " + str(e))
+
+
+def get_yesterdays_date() -> str:
+    """
+    Simple function for getting yesterday's date.
+    :return String:
+    """
+    yesterday = datetime.datetime.now() - datetime.timedelta(1)
+    print("Yesterday is: ", str(yesterday)[0:10])
+    return str(yesterday)[0:10]
+
+
+def file_handler(input_data) -> None:
+    """
+    Open a file in "data" folder and add a time (now) and wind speed.
+    :param input_data:
+    :return None: # should this be a boolean for success / failure?
+    """
+    try:
+        # file_object = open("/data/" + str(datetime.datetime.today())[0:10] + ".txt", 'a')
+        # time_stamp = str(datetime.datetime.now())
+        # file_object.write(time_stamp[0:16] + "," + str(input_data) + ",\n")
+        # file_object.close()
+        with open("/data/" + str(datetime.datetime.today())[0:10] + ".txt", 'a') as fileObject:
+            timeStamp = str(datetime.datetime.now)
+            fileObject.write(f"{timeStamp},{input_data},\n")
+    except Exception as err:
+        print("Problem: ", err)
+    return
+
+
+def open_file(filename):
+    """
+    Find and open a file to read data from it.
+    :param filename:
+    :return Error as string:
+    """
+    output = ""
+    try:
+        output = open(filename, "r")
+    except Exception as err:
+        print("error opening file: ", err)
+    if output is not None:
+        pass
+    else:
+        output = "Error"  # need a better handle than this
+    return output
+
+def sort_dates(input_list) -> list:
+    """
+    Sort the dates of a list from string to YY MM DD HH.
+    Need to check format of the data as it returns integers?
+    :param input_list:
+    :return list:
+    """
+    print("input list: ", input_list)
+    date_output_list = []
+    for i in input_list:
+        date_output_list.append(datetime.datetime(int(i[0] + i[1]),
+                                                  int(i[3] + i[4]),
+                                                  int(i[6] + i[7]),
+                                                  int(i[9] + i[10])))
+    date_output_list.sort()  # this should go in ascending order
+    return date_output_list
+
+
+def read_in_data(filename:str)->list:
+    """
+    Read in data from csv file.
+    """
+    output = []
+    file = open_file(filename)
+    reader = csv.reader(file)
+    for each_row in reader:
+        output.append(each_row)
+    file.close()
+    return output
+
+def reformat_data(input_list:list): # how to declare two list returns?
+    """
+    This will take data in str format "YY-MM-DD HH" and return into (datetime, str)
+    :return: list -> WeatherData(datetime, str)
+    """
+    local_x = []
+    local_y = []
+    output = []
+
+    for gl in input_list:
+        for counter, g in enumerate(gl):
+            if counter % 2 == 0:
+                temp = g.replace('"', '')
+                t = str(temp.lstrip())
+                tmp = str(t[0:2]) + str(t[3:5]) + str(t[6:8])
+                local_x.append(tmp)
+                # print("stuff: {}, {}, {}, hour {}".format(t[0:2], t[3:5], t[6:8], t[9:11]))
+            else:
+                local_y.append(str(g))
+        #output = sort_dates(local_x) # this takes a string yymmdd and returns a datetime format.
+    return local_x, local_y
