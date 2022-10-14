287e81bc17bb5f1eca67258fbe614a6700e7e8cf
local_admin <t@h.com>
2022-10-14 15:59:52 +0100

    Original now works.

--- original.py
+++ original.py
@@ -3,27 +3,28 @@
 try:
     import time
     import RPi.GPIO as GPIO
-    import argparse
-    import Capture
-    import basic_plotting
+    import sys
 except ImportError as e:
     sys.exit("Importing error: " + str(e))
 
 
-interval = 15 #How long we want to wait between loops (seconds)
-windTick = 0 #Used to count the number of times the wind speed input is triggered
+interval = 15  # How long we want to wait between loops (seconds)
+windTick = 0  # Used to count the number of times the wind speed input is triggered
 
-#Set GPIO pins to use BCM pin numbers
+# Set GPIO pins to use BCM pin numbers
 GPIO.setmode(GPIO.BCM)
 
-#Set digital pin 17 to an input and enable the pullup
+# Set digital pin 17 to an input and enable the pull-up
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#Event to detect wind (4 ticks per revolution)
+# Event to detect wind (4 ticks per revolution)
 GPIO.add_event_detect(17, GPIO.BOTH)
-def windtrig(self):
+
+
+def windtrig() -> None:
     global windTick
     windTick += 1
+
 
 GPIO.add_event_callback(17, windtrig)
 
@@ -31,7 +32,7 @@ while True:
 
     time.sleep(interval)
 
-    # Calculate average windspeed over the last 15 seconds
+    # Calculate average wind speed over the last 15 seconds
     windSpeed = (windTick * 1.2) / interval
     windTick = 0
 
