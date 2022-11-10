cf36de148b63ec396279e24e2a85908120f36028
local_admin <t@h.com>
2022-11-10 20:32:03 +0000

    still not ticking though, trying partial()

--- counter.py
+++ counter.py
@@ -17,6 +17,7 @@ try:
     import functions
     import time
     import logging
+    from functools import partial
 except Exception as e:
     print("importing error: ", e)
 
@@ -28,11 +29,8 @@ logging.basicConfig(filename="logging/log.txt", level=logging.DEBUG, format="%(a
 class WindMonitor:
     coreDataFilePath = "Use a configuration or variable"
 
-    def add_count(self):
-        logging.debug('add_count()')
-        self.count += 1
 
-    def __init__(self, intervalNumber: int, pinNumber: int):
+    def __init__(self, intervalNumber: int, pinNumber: int) -> None:
         self.PIN = pinNumber
         self.interval = intervalNumber
         logging.info('Initiating the weather monitor')
@@ -45,13 +43,21 @@ class WindMonitor:
         logging.info('setup()')
         GPIO.add_event_detect(self.PIN, GPIO.BOTH)
         logging.info('add_event_detect()')
-        GPIO.add_event_callback(self.PIN, self.add_count)
+        GPIO.add_event_callback(self.PIN, partial(self.add_count))
+
+
+    def add_count(self):
+        logging.debug('add_count()')
+        self.count += 1
+
 
     def show_count(self):
         return self.count
 
+
     def get_interval(self):
         return self.interval
+
 
     def reset(self):
         self.count = 0
