09ca58cfdb0d23e026596e8c9b17518af5370238
local_admin <t@h.com>
2022-11-11 11:48:25 +0000

    tackling original

--- original.py
+++ original.py
@@ -7,27 +7,32 @@ try:
 except ImportError as e:
     sys.exit("Importing error: " + str(e))
 
+
+global windTick
+global interval
+interval = 10
 windTick = 0  # Used to count the number of times the wind speed input is triggered
+
+def system_check():
+    print("GPIO info", GPIO.RPI_INFO)
 
 def windtrig() -> None:
     global windTick
     windTick += 1
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(17, GPIO.BOTH)
+GPIO.add_event_callback(17, windtrig)
 
-def main_function(interval: int) -> None:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(17, GPIO.BOTH)
-    GPIO.add_event_callback(17, windtrig)
+while True:
+    time.sleep(interval)
+    windSpeed = (windTick * 1.2) / interval
+    windTick = 0
 
-    while True:
-        time.sleep(interval)
-        windSpeed = (windTick * 1.2) / interval
-        windTick = 0
+    print("Wind Speed: ", windSpeed, " KPH")
 
-        print("Wind Speed: ", windSpeed, " KPH")
-
-
+"""
 if __name__ == "__main__":
-    windTick = 0  # Used to count the number of times the wind speed input is triggered
-    main_function(10)
+    pass
+"""
