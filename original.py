cbaf45ce27ebec497d9e26221e98dbf89ab0c62d
local_admin <t@h.com>
2022-11-11 11:35:17 +0000

    tacking original

--- original.py
+++ original.py
@@ -7,33 +7,27 @@ try:
 except ImportError as e:
     sys.exit("Importing error: " + str(e))
 
-
-interval = 15  # How long we want to wait between loops (seconds)
 windTick = 0  # Used to count the number of times the wind speed input is triggered
-
-# Set GPIO pins to use BCM pin numbers
-GPIO.setmode(GPIO.BCM)
-
-# Set digital pin 17 to an input and enable the pull-up
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Event to detect wind (4 ticks per revolution)
-GPIO.add_event_detect(17, GPIO.BOTH)
-
 
 def windtrig() -> None:
     global windTick
     windTick += 1
 
 
-GPIO.add_event_callback(17, windtrig)
+def main_function(interval: int) -> None:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(17, GPIO.BOTH)
+    GPIO.add_event_callback(17, windtrig)
 
-while True:
+    while True:
+        time.sleep(interval)
+        windSpeed = (windTick * 1.2) / interval
+        windTick = 0
 
-    time.sleep(interval)
+        print("Wind Speed: ", windSpeed, " KPH")
 
-    # Calculate average wind speed over the last 15 seconds
-    windSpeed = (windTick * 1.2) / interval
-    windTick = 0
 
-    print("Wind Speed: ", windSpeed, " KPH")
+if __name__ == "__main__":
+    windTick = 0  # Used to count the number of times the wind speed input is triggered
+    main_function(10)
