35e2e1b354a5f11a36e8df7dc21151e72ba15787
jonathan.l <jonathan.l@hdcorp.co.uk>
2023-10-06 16:26:08 +0100

    file open config.json error

--- src/functions.py
+++ src/functions.py
@@ -20,7 +20,7 @@ def get_config() -> config_data:
     """
     Get the config from a json file and return an object class of that data.
     """
-    location = "config.json"
+    location = "src/config.json"
     config_data_object = config_data()
 
     if location.lower().endswith('.json'):
