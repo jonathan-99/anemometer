fb26818faad7dc44f7d0a9b0a8a16b9ff7b53105
jonathan.l <jonathan.l@hdcorp.co.uk>
2024-01-24 15:19:53 +0000

    bug hunting. not the answer. split out FileNotFoundError and JSONDecodeError. also added a directory listing to understand if it can see config.json file

--- src/class_file.py
+++ src/class_file.py
@@ -31,6 +31,7 @@ class ConfigData:
             logging.error("Error reading JSON file: {}".format(err))
             return {"Error": "Invalid JSON format or empty file"}
 
+
     def __init__(self, filename='config.json'):
         logging.debug("--This is the path -- {} - {} - {}".format(os.path.isfile(filename),
                                                                   os.path.exists(filename), filename))
