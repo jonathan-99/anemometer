8469e57262a31287e1932f31809a62e9b914ce39
jonathan.l <jonathan.l@hdcorp.co.uk>
2024-02-29 20:56:32 +0000

    chasing do_unittests.sh with a coverage error

--- jenkins_scripts/do_unittests.sh
+++ jenkins_scripts/do_unittests.sh
@@ -3,9 +3,11 @@
 set -eu -o pipefail # fail on error and report it, debug all lines
 
 sudo -n true
-test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this s"
 
 container_name='anemometer-test'
+
+CONTAINER_ID=$(docker ps -q --filter "name=${container_name}")
 
 # PEP8 report
 docker exec $CONTAINER_ID sh -c 'command -v pep8 >/dev/null 2>&1 || { apt-get update && apt-get install -y python3-pep8; } && pep8 /path/to/your/directory'
@@ -14,7 +16,11 @@ docker exec $CONTAINER_ID sh -c 'command -v pep8 >/dev/null 2>&1 || { apt-get up
 # need to change test_all to test_*
 docker exec $CONTAINER_ID python3 -m unittest discover -s 'testing/' -v -p 'test_all.py'
 
+# Install coverage package if not already installed
+docker exec $CONTAINER_ID sh -c 'command -v coverage >/dev/null 2>&1 || { echo >&2 "Coverage is not installed. Installing..."; apt-get update && apt-get install -y python3-coverage; }'
+
 # this is a coverage report
 docker exec $CONTAINER_ID coverage html -d coverage_report
 
+# Install pip if not already installed and generate requirements.txt
 docker exec $CONTAINER_ID sh -c 'command -v pip >/dev/null 2>&1 || { echo >&2 "pip is not installed. Installing..."; apt-get update && apt-get install -y python3-pip; }; pip freeze | tee requirements.txt'
